a1d00eec8d062c3f24fd75957824ee3f90a14965
weather-man-dan <danielrgalante@gmail.com>
2025-05-22 16:04:10 -0400

    Update model.py with functions

--- model.py
+++ model.py
@@ -1,1 +1,24 @@
 # This file will contain the model that I use to predict future outcomes (forecasted energy demand)
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+def train_linear_model(df, feature_cols, target_col):
+    X = df[feature_cols]
+    y = df[target_col]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    return model, X_test, y_test
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    return r2, mae, y_pred
+

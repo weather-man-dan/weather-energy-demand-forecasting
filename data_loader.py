c42a61c797abcf16eb6b2529befd32b3f374fe83
weather-man-dan <danielrgalante@gmail.com>
2025-05-22 16:03:35 -0400

    Update data_loader.py with functions

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,12 @@
 # This file will contain the python code to import relevant data sets (NOAA, EIA Data, etc)
+
+
+import pandas as pd
+
+def load_noaa_data(filepath: str) -> pd.DataFrame:
+    df = pd.read_csv(filepath, parse_dates=["date"])
+    return df
+
+def load_eia_data(filepath: str) -> pd.DataFrame:
+    df = pd.read_csv(filepath, parse_dates=["date"])
+    return df

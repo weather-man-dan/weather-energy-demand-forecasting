d82f05f14de4e6ba8046a10af8242336f1dd3eca
weather-man-dan <danielrgalante@gmail.com>
2025-05-22 16:03:01 -0400

    Update preprocess.py with functions

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,15 @@
 # This file will contain functions to clean, merge, and transform data
+
+import pandas as pd
+
+def merge_and_clean_data(df_weather: pd.DataFrame, df_energy: pd.DataFrame) -> pd.DataFrame:
+    df = pd.merge(df_weather, df_energy, on="date")
+    df = df.dropna()
+    return df
+
+def create_features(df: pd.DataFrame) -> pd.DataFrame:
+    df["HDD_rolling"] = df["HDD"].rolling(window=7).mean()
+    df["lag_demand"] = df["demand"].shift(1)
+    df = df.dropna()
+    return df
+
